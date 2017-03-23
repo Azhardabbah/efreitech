3bf25b9f3e285bdbae0190aa279387f38c9e7c0b
alamoulie <alamoulie@nextperf.com>
2017-03-23 12:25:03 +0100

    tp02 joke

---
+++ src/jour1/TP02/tp02.java
@@ -0,0 +1,50 @@
+package jour1.TP02;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.util.Iterator;
+
+/**
+ * . jour1.TP02
+ *
+ * @author alamoulie
+ * @date 22/03/17
+ */
+public class tp02 {
+
+
+
+    private static final String JSON = "[\n" +
+            "  {\n" +
+            "    \"name\" : \"Tupac Shakur\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Big Small\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Dr Dre\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"EPMD\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Eminem\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Ice Cube\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"50 cent\"\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"name\" : \"Moob Deep\"\n" +
+            "  }\n" +
+            "]";
+
+    public static void main(String[] args) {
+
+    }
+}
